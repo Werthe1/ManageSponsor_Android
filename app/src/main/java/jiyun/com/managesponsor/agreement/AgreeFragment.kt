f568f536dc66309f0e6268d163d9fed29c929de4
jiyunn <jygr0309@gmail.com>
2018-10-22 22:14:45 +0900

    #37-약정 상세화면 이동 기능 추가

--- app/src/main/java/jiyun/com/managesponsor/agreement/AgreeFragment.kt
+++ app/src/main/java/jiyun/com/managesponsor/agreement/AgreeFragment.kt
@@ -1,6 +1,7 @@
 package jiyun.com.managesponsor.agreement
 
 
+import android.app.Activity
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -8,14 +9,18 @@ import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
+import io.reactivex.disposables.CompositeDisposable
 import jiyun.com.managesponsor.R
+import jiyun.com.managesponsor.agreementDetail.AgreeDetailActivity
 import jiyun.com.managesponsor.data.Agreement
 import jiyun.com.managesponsor.databinding.AgreeFragBinding
+import jiyun.com.managesponsor.util.startOtherActivity
 import java.util.*
 
 class AgreeFragment : Fragment() {
 
     private lateinit var binding: AgreeFragBinding
+    private val disposables = CompositeDisposable()
 
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
@@ -38,11 +43,26 @@ class AgreeFragment : Fragment() {
 
             adapter = AgreeAdapter().apply {
                 addItems(mutableListOf(
-                        Agreement("2015101820-1", Date(), Date(), 20000, "이지윤", "1101231254561", 20, "CMS",
-                        "국민", "233002-04-131313", "이지윤", "920413"),
-                        Agreement("2012101820-1", Date(), Date(), 15000, "윤지이", "1231231254561", 25, "급여공제",
-                                "우리", "1302-04-131313", "윤지이", "960212")))
+                        Agreement("2018-0044-99", Date(), Date(),
+                                15000, "이지윤" , "11", "010-1234-5678","성공회대학교 소프트웨어공학 발전기금",20,
+                                "CMS", "국민", "233002-04-133003", "이지윤", "920413", "비고\n비고입니다."),
+                        Agreement("2018-0044-99", Date(), Date(),
+                                15000, "이지윤" , "11", "010-4567-8970","성공회대학교 소프트웨어공학 발전기금",20,
+                                "CMS", "국민", "233002-04-133003", "이지윤", "920413", "비고\n비고입니다.")))
+
+
+                disposables.add(
+                        clickSubject.subscribe {
+                            startOtherActivity(context, AgreeDetailActivity())
+                        }
+                )
             }
         }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        disposables.dispose()
     }
 }
--- app/src/main/java/jiyun/com/managesponsor/agreement/AgreeViewHolder.kt
+++ app/src/main/java/jiyun/com/managesponsor/agreement/AgreeViewHolder.kt
@@ -4,11 +4,19 @@ package jiyun.com.managesponsor.agreement
 import android.view.View
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
+import io.reactivex.Observable
+import jiyun.com.managesponsor.data.Agreement
+import jiyun.com.managesponsor.data.Schedule
 import jiyun.com.managesponsor.databinding.ItemAgreementBinding
 
 class AgreeViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView) {
 
     val binding  : ItemAgreementBinding?= DataBindingUtil.bind(itemView)
 
-
+    fun getClickObservable(item: Agreement): Observable<Agreement> =
+            Observable.create { emitter ->
+                itemView.setOnClickListener {
+                    emitter.onNext(item)
+                }
+            }
 }
