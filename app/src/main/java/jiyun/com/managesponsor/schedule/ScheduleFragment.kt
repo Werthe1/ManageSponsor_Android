479babb3a1ef96a712b386404b7efb4f674cf371
jiyunn <jygr0309@gmail.com>
2018-10-22 22:00:07 +0900

    #30-일정 추가 이벤트 적용

--- app/src/main/java/jiyun/com/managesponsor/schedule/ScheduleFragment.kt
+++ app/src/main/java/jiyun/com/managesponsor/schedule/ScheduleFragment.kt
@@ -7,19 +7,24 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
 import androidx.recyclerview.widget.LinearLayoutManager
+import io.reactivex.disposables.CompositeDisposable
 import jiyun.com.managesponsor.R
 import jiyun.com.managesponsor.data.Schedule
 import jiyun.com.managesponsor.databinding.ScheduleFragBinding
+import jiyun.com.managesponsor.scheduledetail.ScheduleDetailFragment
+import java.lang.NullPointerException
 import java.util.*
 
-class ScheduleFragment : Fragment() {
+class ScheduleFragment : Fragment(), ScheduleNavigator {
 
     private lateinit var binding: ScheduleFragBinding
-
+    private val disposables = CompositeDisposable()
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         binding = DataBindingUtil.inflate(inflater, R.layout.schedule_frag, container, false)
+        binding.viewModel = ScheduleViewModel(this)
 
         return binding.root
     }
@@ -36,14 +41,32 @@ class ScheduleFragment : Fragment() {
             setHasFixedSize(true)
             layoutManager = LinearLayoutManager(this@ScheduleFragment.context)
 
+
             adapter = ScheduleAdapter().apply {
                 addItems(mutableListOf(
                         Schedule(Date(), "제 123123회 후원인의 밤 행사 (주최 : 동문회)\n성공회대 일만관 2401", "이지윤", 1),
                         Schedule(Date(), "후원인 2019 달력 발송 및 소식지 발송", "김혜리", 0),
                         Schedule(Date(), "우편물 및 기념품 발송 ", "김혜리", 0),
                         Schedule(Date(), "김정호 고문님 CEO 장학금 요청 - 김태윤 학생 복학 확인했음. 지원 김태윤 학생에게  계속하는지 여부 확인할 것.", "김혜리", 0)))
+
+
+                disposables.add(
+                        clickSubject.subscribe {
+                            ScheduleDetailFragment().show(activity?.supportFragmentManager, "preview")
+                        }
+                )
             }
         }
+    }
+
+    override fun getSupportFragmentManager(): FragmentManager = activity?.supportFragmentManager
+            ?: throw  NullPointerException("Support manager is null")
+
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        disposables.dispose()
     }
 
 }
